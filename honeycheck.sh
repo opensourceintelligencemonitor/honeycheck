9122c17473f4038f56ccf9c9e34fcf771ca18005
Kelly Hart <opensourceintelligencemonitor@zoho.com>
2016-04-16 14:42:51 +1000

    Update honeycheck.sh

--- honeycheck.sh
+++ honeycheck.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 ip=$1
+if [[ $# -eq 0 ]] ; then
+echo '-------------------------------'    
+echo 'please supply filename of ip's'
+echo '-------------------------------'
+    exit 0
+fi
 while read line; do sudo shodan honeyscore "$line"; done <$ip
